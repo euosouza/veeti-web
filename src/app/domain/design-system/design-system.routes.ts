7176f9efab3ed6d84e9a703f8f01283e8965e0fa
Victor Souza <euosouza@gmail.com>
2025-12-30 14:59:09 -0300

    feature: criando pagina e definindo tipografia do ds

--- src/app/domain/design-system/design-system.routes.ts
+++ src/app/domain/design-system/design-system.routes.ts
@@ -4,5 +4,9 @@ export const routesDesignSystem: Routes = [
   {
     path: "",
     loadComponent: () => import("./pages/home/home.page").then((m) => m.HomePage)
+  },
+  {
+    path: "tipografia",
+    loadComponent: () => import("./pages/typography/typography.page").then((m) => m.TypographyPage)
   }
 ];
---
+++ src/app/domain/design-system/pages/typography/typography.page.ts
@@ -0,0 +1,79 @@
+import { Component } from "@angular/core";
+
+interface EscalaTitulo {
+  token: "H1" | "H2" | "H3" | "H4" | "H5" | "H6";
+  specs: {
+    size: string;
+    weight: string;
+    classText: string;
+  };
+  preview: string;
+  element?: string;
+}
+
+@Component({
+  selector: "app-typography",
+  imports: [],
+  templateUrl: "./typography.page.html"
+})
+export class TypographyPage {
+  escalaTitulos: EscalaTitulo[] = [
+    {
+      token: "H1",
+      specs: {
+        size: "36px / 40px",
+        weight: "Bold (700)",
+        classText: "text-4xl font-bold"
+      },
+      preview: "Prontuário do Paciente",
+      element: `<h1 class="text-4xl font-bold text-text-main dark:text-white leading-tight">
+        Prontuário do Paciente
+      </h1>`
+    },
+    {
+      token: "H2",
+      specs: {
+        size: "30px / 36px",
+        weight: "SemiBold (600)",
+        classText: "text-3xl font-semibold"
+      },
+      preview: "Histórico Clínico"
+    },
+    {
+      token: "H3",
+      specs: {
+        size: "24px / 32px",
+        weight: "SemiBold (600)",
+        classText: "text-2xl font-semibold"
+      },
+      preview: "Vacinas Agendadas"
+    },
+    {
+      token: "H4",
+      specs: {
+        size: "20px / 28px",
+        weight: "Medium (500)",
+        classText: "text-xl font-medium"
+      },
+      preview: "Dados do Tutor"
+    },
+    {
+      token: "H5",
+      specs: {
+        size: "18px / 28px",
+        weight: "Medium (500)",
+        classText: "text-lg font-medium"
+      },
+      preview: "Exames Recentes"
+    },
+    {
+      token: "H6",
+      specs: {
+        size: "14px / 24px",
+        weight: "Medium (500)",
+        classText: "text-md font-medium"
+      },
+      preview: "Dados do Paciente"
+    }
+  ];
+}
